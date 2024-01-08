dea5b8699f0cab4af8d5d7c6036388e86a96b392
pradipnemane <pnemane@uncc.edu>
2024-01-07 22:41:55 -0500

    added server api

--- README.md
+++ README.md
@@ -16,3 +16,44 @@ Edit this repo to answer these two questions using any technologies you'd like, 
 1. Clone the repo
 2. Make a PR with your changes in your repo
 3. Email your github repository to robert@bequest.finance
+
+
+### Prerequisites
+
+**User Authentication:**
+
+   - Use the default credentials for initial testing:
+     - Username: `admin`
+     - Password: `admin`
+
+### 1. How does the client ensure that their data has not been tampered with
+
+#### Encryption and Hashing
+
+Data is stored securely by applying encryption for confidentiality and hashing for integrity verification.
+1. Data is encrypted, adding a layer of confidentiality.
+2. Hashing is applied to the data, creating a hash value that serves as a fingerprint for integrity verification.
+
+#### JWT Token Authentication
+
+1. User authentication is handled using JSON Web Tokens (JWT). Only authenticated users with valid tokens can access and modify data.
+
+#### Access Control
+
+1. Access to data is controlled by requiring user authentication, and only authenticated users with the appropriate JWT token can retrieve or update data.
+
+#### Data Update Verification
+
+1. Before allowing a data update, the client verifies the integrity of the data by comparing the hash of the current data with the hash stored in the database. This step adds an extra layer of protection against tampering.
+
+### 2. If the data has been tampered with, how can the client recover the lost data?
+
+#### Data Recovery API
+
+1. A dedicated API is available for data recovery, but it is accessible only to valid and authenticated users. This ensures that the recovery process is controlled and secure.
+
+### 3. Verify Data Correctness
+
+#### Hash Verification
+1. To verify the correctness of data, the client can send both the original data and its encrypted version.
+2. The server decrypts the encrypted data, calculates its hash, and compares it with the provided hash value. If they match, the data is considered unaltered.
--- server/app.ts
+++ server/app.ts
@@ -1,24 +1,136 @@
 import express from "express";
 import cors from "cors";
+import jwt from "jsonwebtoken";
+import {
+  verifyUser,
+  hashData,
+  SECRET_KEY,
+  readPrivateAndPublicKey,
+  encryptData,
+  decryptData,
+} from "./util";
+
 
 const PORT = 8080;
 const app = express();
-const database = { data: "Hello World" };
+
+// database
+const database = { data: "Hello World", hash: "", encryptedData: "" };
+// backup database
+const backupDatabase = { data: "Hello World", hash: "", encryptedData: "" };
 
 app.use(cors());
 app.use(express.json());
 
-// Routes
+readPrivateAndPublicKey();
 
-app.get("/", (req, res) => {
-  res.json(database);
+// Init data
+const initData = () => {
+  database.hash = hashData(database.data);
+  backupDatabase.hash = hashData(database.data);
+  const { encryptedData } = encryptData(database.data);
+  database.encryptedData = encryptedData;
+  backupDatabase.encryptedData = encryptedData;
+};
+
+// this route is used for getting the data
+app.get("/", verifyUser, (req, res) => {
+  res
+    .status(200)
+    .json({ data: database.data, encryptedData: database.encryptedData });
 });
 
-app.post("/", (req, res) => {
-  database.data = req.body.data;
-  res.sendStatus(200);
+app.post("/login", (req, res) => {
+  const { username, password } = req.body;
+  if (username === "admin" && password === "admin") {
+    const token = jwt.sign({ username: username }, SECRET_KEY || "", {
+      expiresIn: "1h",
+    });
+    res.status(200).json({ token: token });
+  } else {
+    res.status(400).json({ msg: "Invalid credentials" });
+  }
+});
+
+app.post("/", verifyUser, (req, res) => {
+  const clientData = req.body.data;
+  const encryptedData = req.body.encryptedData;
+  if (!clientData || !encryptedData) {
+    res.status(400).json({ msg: "Invalid data" , status: false});
+    return;
+  }
+  try {
+    const { data, hash } = decryptData(encryptedData);
+    const serverHashHex = database.hash;
+
+    if (serverHashHex === hash) {
+      database.encryptedData = encryptData(clientData).encryptedData;
+      database.data = clientData;
+      database.hash = hashData(clientData);
+      backupDatabase.encryptedData = encryptData(clientData).encryptedData;
+      backupDatabase.data = clientData;
+      backupDatabase.hash = hashData(clientData);
+      res.json({ data: database.data, msg: "Data Updated Successfully" , status: true});
+    } else {
+      res.sendStatus(400);
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({ msg: "Data is tempered or invalid" , status: false});
+  }
+});
+
+app.post("/encrypt", (req, res) => {
+  try {
+    const data = req.body.data;
+    const { encryptedData, hash } = encryptData(data);
+    res.json({ encryptedData: encryptedData });
+  } catch (error: any) {
+    console.error("Error encrypting data:", error.message);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+app.post("/decrypt", (req, res) => {
+  try {
+    const encryptedData = req.body.encryptedData;
+    try {
+      const { data } = decryptData(encryptedData);
+      res.json({ data: data });
+    } catch (error) {}
+  } catch (error: any) {
+    console.error("Error decrypting data:", error.message);
+    res.status(400).json({ error: "Invalid data" });
+  }
+});
+// Verify routes used for verifying data
+app.post("/verify", (req, res) => {
+  const clientData = req.body.data; // normal text
+  const encrypted = req.body.encryptedData; // encrypted text
+  const clientHash = hashData(clientData);
+  const { data, hash } = decryptData(encrypted);
+  const serverHashHex = database.hash; // data base hash of original data
+  if (hash === clientHash && serverHashHex === hash && data === clientData) {
+    res.status(200).json({
+      msg: "Data is verified",
+      status: true
+    });
+  } else {
+    res.status(400).json({ msg: "Data is tempered or invalid", status: false });
+  }
+});
+
+// Backup routes used for restoring data
+app.get("/retrieve-backup-data", verifyUser, (req, res) => {
+  const { data } = decryptData(backupDatabase.encryptedData);
+  const encryptedData = encryptData(data);
+  res
+    .status(200)
+    .json({ data: data, encryptedData: encryptedData.encryptedData });
 });
 
 app.listen(PORT, () => {
+  initData();
+  readPrivateAndPublicKey();
   console.log("Server running on port " + PORT);
 });
